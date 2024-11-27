Earewien/godot-traits

commit e8ceac27e99ef736985a91f1adf9581214e07560

GTraitsContainer: ensure that it is still inside SceneTree when child_order_changed is triggered (#13)
* Fixed typo in required container type.

Check for Node3D to set container as gtraits_container_3d.tscn

* Check if container is in tree before updating its children

addons/godot-traits/core/container/gtraits_container.gd
@@ -65,10 +65,14 @@ func _set_trait_container_type() -> void:
     set_meta(GTraitsStorage.META_KEY_CONTAINER_TYPE, "Node")
 
 func _on_child_order_changed() -> void:
-    for child in get_children():
-        # Check if invalid node is present or not
-        if child.get_meta(_META_KEY_NOT_A_TRAIT, false):
-            continue
+	# Check if the container is still in the tree
+	if not is_inside_tree():
+		return
+	
+	for child in get_children():
+		# Check if invalid node is present or not
+		if child.get_meta(_META_KEY_NOT_A_TRAIT, false):
+			continue
 
         # Flag invalid node
         if not GTraitsTypeOracle.get_instance().is_trait(child.get_script()):

commit 044955798706ecc115e30de502aca1c6847611a0

Fix bug where added traits using trait container not in SceneTree are not declared as traits
Regression from https://github.com/Earewien/godot-traits/pull/13

addons/godot-traits/core/container/gtraits_container.gd
@@ -51,6 +51,9 @@ func _init() -> void:
     # This callback is called before the trait _ready function and after it's _init function
     # so it's perfect to invoke the trait _initialize function
     child_order_changed.connect(_on_child_order_changed)
+    # Children added to this container outside SceneTree are not yet declared as trait
+    # So when this container is added to the scene tree, declare them as trait if needed
+    tree_entered.connect(_on_child_order_changed)
 
 #------------------------------------------
 # Public functions